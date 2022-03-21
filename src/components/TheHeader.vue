b79adceaa23d43315ec40e71594aa568b0cc93e9
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2022-03-21 01:08:19 -0600

    initial setup

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="flex gap-4 bg-green-200 py-4">
+    <div class="flex gap-2 items-center ml-2">
+      <img src="../assets/logo.png" alt="Vue Logo" class="w-16" />
+      <span class="text-2xl font-semibold text-black">Day 15</span>
+    </div>
+  </header>
+</template>
