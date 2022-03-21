a6b2ea952e1182974caa3fa3de94157fcf033ba1
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2022-03-21 01:14:11 -0600

    added props for the title

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,7 +2,16 @@
   <header class="flex gap-4 bg-green-200 py-4">
     <div class="flex gap-2 items-center ml-2">
       <img src="../assets/logo.png" alt="Vue Logo" class="w-16" />
-      <span class="text-2xl font-semibold text-black">Day 15</span>
+      <span class="text-2xl font-semibold text-black">{{ props.title }}</span>
     </div>
   </header>
 </template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Day 15'
+  }
+});
+</script>
